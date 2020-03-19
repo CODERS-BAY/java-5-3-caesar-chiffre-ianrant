f6020259b407a7859da939c67dae53766fc5dc61
Ben Leitl <ben.leitl@me.com>
2020-03-19 18:18:53 +0100

    Fix bugs: I forgot to add a known issue to the last commit and tried to find work it out... there is still a glitch I can't seem to fix. Please try with an offset of 4 and the following phrase: "Xylophon Lyon LexLuther Xander Waterworld Saxophon SuperSweetSixteen"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void main(String[] args) {
@@ -19,49 +18,34 @@ public class Main {
 
         System.out.println("Please enter a sentence that you want to have ciphered:");
         String textInput = stringScanner.nextLine();
-
         char[] characterArray = textInput.toCharArray();
 
         for (int i =0; i <characterArray.length; i++){
             int temp = characterArray[i];
 
+            System.out.print(characterArray[i] + ": ");
+            System.out.print("current temp = " + temp + " ==> ");
+
             temp += offset;
 
+            System.out.print("temp+offset = " + temp + " ==> ");
+
             // if value of temp exceeds the unicode range of capital letters or small letters, it the counter
-            // goes back to A or a and counts up (proportionately from there)
-            if ((temp > 90) | (temp > 122)){
+            // goes back to A or a and cunts up (proportionately from there)
+            if ((temp > 90 && temp < 96) ^ (temp < 97 && temp > 122)) {
                 temp -= 26;
-            }
+            } /*else if (){
+                temp -= 26;
+            }*/
 
+            System.out.print(" corrected temp = " + temp + " ");
+            System.out.println();
+
+
+
+            /*
             characterArray[i] = (char)temp;
-            System.out.print(characterArray[i]);
+            System.out.print(characterArray[i]);*/
         }
-
-
-        // when the decimal value of a character is between 65 and 90 (for capital letters)
-        // or between 97 and 122 (for small letters) everything is fine
-        // but if they exceed, they must be subtracted by 26 to fit into the "allowed" range again
-
-
-
-
-
-
-        /*for (int k = 0; k <= preCipher.length; k++){
-            String finalCipher = ((char)(preCipher[k]+cipherNumber)) ;
-            System.out.print();
-        }*/
-
-
-
-
-
-
-
-        /*char[] sign  = {'C', 'D'} ;
-
-        System.out.println((char)(sign[0] + 4));
-        System.out.println((char)(sign[1]+ 7));*/
     }
-
 }
